fb81e50d760c0c50e8bffd60ccd9876306e8c7dc
CoolNight99 <timmyscottmy@gmail.com>
2025-04-21 16:50:19 +0800

    changed total score to dynamically update based on data

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-import data from "../data.json" with { type: "json" };
+import data from "./data.json" with { type: "json" };
 
+const result = document.querySelector(".result");
 const summaryCards = document.querySelector(".summary-cards");
 const bgColors = ["hsla(0, 100%, 67%, 10%)", "hsla(39, 100%, 56%, 10%)", "hsla(166, 100%, 37%, 10%)", "hsla(234, 85%, 45%, 10%)"]
 const textColors = ["hsl(0, 100%, 67%)", "hsl(39, 100%, 56%)", "hsl(166, 100%, 37%)", "hsl(234, 85%, 45%)"]
+let totalScore = 0;
 
 for (let i = 0; i < data.length; i++) {
     summaryCards.innerHTML += `
@@ -14,5 +16,11 @@ for (let i = 0; i < data.length; i++) {
                                     <p class="score m-0"><span class="score-value">${data[i].score}</span> / 100</p>
                                 </div>
 
-                            `
+                            `;
+
+    totalScore += data[i].score;
 }
+
+const avg = (totalScore / data.length).toFixed(0)
+
+result.innerHTML = `${avg}`;
